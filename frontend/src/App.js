11aafbff050ca3944fd23ea41fd7b47f5ef96caa
Ajey Venkataraman <avenkataraman@magnite.com>
2024-12-01 12:19:56 -0800

    added dashboard components

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TradingDashboard from './components/ui/TradingDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <TradingDashboard />
+    </div>
+  );
+}
+
+export default App;
